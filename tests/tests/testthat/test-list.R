ab745b321a5f28ba3e99fe295c8f0bb86cb7e085
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-09-14 22:18:11 -0700

    Test that unserialization of DFs works as expected.

--- tests/tests/testthat/test-list.R
+++ tests/tests/testthat/test-list.R
@@ -15,3 +15,17 @@ test_that("list loading works as expected", {
         expect_identical(roundtrip, y)
     }
 })
+
+test_that("data frame loading works as expected", {
+    tmp <- tempfile(fileext=".rds")
+    df <- data.frame(xxx=runif(19), YYY=sample(letters, 19), ZZZ=rbinom(19, 1, 0.4) == 0)
+    saveRDS(df, file=tmp)
+    roundtrip <- rds2cpp:::parse(tmp)
+    expect_identical(roundtrip, df)
+
+    # Works with row names.
+    rownames(df) <- paste0("FOO-", LETTERS[1:19])
+    saveRDS(df, file=tmp)
+    roundtrip <- rds2cpp:::parse(tmp)
+    expect_identical(roundtrip, df)
+})
