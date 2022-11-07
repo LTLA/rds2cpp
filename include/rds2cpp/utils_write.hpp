1b1ffc7082c50da15f3fa8c924bea90773021652
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-11-06 22:03:02 -0800

    Avoid unnecessary copies for big-endian machines.
    Reduce the growth of the buffer when saving strings.

--- include/rds2cpp/utils_write.hpp
+++ include/rds2cpp/utils_write.hpp
@@ -42,6 +42,22 @@ inline void inject_string(const char* ptr, size_t n, std::vector<unsigned char>&
     buffer.insert(buffer.end(), p, p + n);
 }
 
+template<bool has_attributes, class Object>
+void inject_header(Object& vec, std::vector<unsigned char>& buffer) {
+    buffer.push_back(0);
+    buffer.push_back(0);
+
+    if constexpr(has_attributes) {
+        buffer.push_back(vec.attributes.names.empty() ? 0 : 2);
+    } else {
+        buffer.push_back(0);
+    }
+
+    // cast from enum should be safe, as SEXPTypes are also unsigned chars.
+    buffer.push_back(static_cast<unsigned char>(vec.type())); 
+}
+
+
 }
 
 #endif
--- include/rds2cpp/write_atomic.hpp
+++ include/rds2cpp/write_atomic.hpp
@@ -15,36 +15,36 @@ namespace rds2cpp {
 namespace atomic_internal {
 
 template<class Vector>
-void set_header(Vector& vec, std::vector<unsigned char>& buffer) {
-    buffer.resize(4);
-    buffer[0] = 0;
-    buffer[1] = 0;
-    buffer[2] = (vec.attributes.names.empty() ? 0 : 2);
-    buffer[3] = static_cast<unsigned char>(vec.type()); // cast from enum should be safe, as SEXPTypes are also unsigned chars.
+void set_vector_header(Vector& vec, std::vector<unsigned char>& buffer) {
+    buffer.clear();
+    inject_header<true>(vec, buffer);    
 }
 
 template<class Vector, class Writer>
 void write_integer_or_logical_body(const RObject* obj, Writer& writer, std::vector<unsigned char>& buffer) {
     const auto& vec = *reinterpret_cast<const Vector*>(obj);
-    set_header(vec, buffer);
+    set_vector_header(vec, buffer);
 
     const auto& values = vec.data;
     auto len = values.size();
     inject_length(len, buffer);
+    writer.write(buffer.data(), buffer.size());
 
     auto ptr = reinterpret_cast<const unsigned char*>(values.data());
     constexpr size_t width = 4;
-    size_t sofar = buffer.size();
-    buffer.insert(buffer.end(), ptr, ptr + len * width);
+    size_t nbytes = len * width;
 
     if (little_endian()) {
-        auto copy = buffer.data() + sofar;
+        buffer.clear();
+        buffer.insert(buffer.end(), ptr, ptr + nbytes);
+        auto copy = buffer.data();
         for (size_t n = 0; n < len; ++n, copy += width) {
             std::reverse(copy, copy + width);
         }
+        writer.write(buffer.data(), buffer.size());
+    } else {
+        writer.write(ptr, nbytes);
     }
-
-    writer.write(buffer.data(), buffer.size());
 }
 
 }
@@ -62,84 +62,86 @@ void write_logical(const RObject* obj, Writer& writer, std::vector<unsigned char
 template<class Writer>
 void write_double(const RObject* obj, Writer& writer, std::vector<unsigned char>& buffer) {
     const auto& vec = *reinterpret_cast<const DoubleVector*>(obj);
-    atomic_internal::set_header(vec, buffer);
+    atomic_internal::set_vector_header(vec, buffer);
 
     const auto& values = vec.data;
     auto len = values.size();
     inject_length(len, buffer);
+    writer.write(buffer.data(), buffer.size());
 
     auto ptr = reinterpret_cast<const unsigned char*>(values.data());
     constexpr size_t width = 8;
-    size_t sofar = buffer.size();
-    buffer.insert(buffer.end(), ptr, ptr + len * width);
+    size_t nbytes = len * width;
     
     if (little_endian()) {
-        auto copy = buffer.data() + sofar;
+        buffer.clear();
+        buffer.insert(buffer.end(), ptr, ptr + nbytes);
+        auto copy = buffer.data();
         for (size_t n = 0; n < len; ++n, copy += width) {
             std::reverse(copy, copy + width);
         }
+        writer.write(buffer.data(), buffer.size());
+    } else {
+        writer.write(ptr, nbytes);
     }
-
-    writer.write(buffer.data(), buffer.size());
 }
 
 template<class Writer>
 void write_raw(const RObject* obj, Writer& writer, std::vector<unsigned char>& buffer) {
     const auto& vec = *reinterpret_cast<const RawVector*>(obj);
-    atomic_internal::set_header(vec, buffer);
+    atomic_internal::set_vector_header(vec, buffer);
 
     const auto& values = vec.data;
     auto len = values.size();
     inject_length(len, buffer);
-
-    auto ptr = reinterpret_cast<const unsigned char*>(values.data());
-    size_t sofar = buffer.size();
-    buffer.insert(buffer.end(), ptr, ptr + len);
-
     writer.write(buffer.data(), buffer.size());
+
+    writer.write(values.data(), len);
 }
 
 template<class Writer>
 void write_complex(const RObject* obj, Writer& writer, std::vector<unsigned char>& buffer) {
     const auto& vec = *reinterpret_cast<const ComplexVector*>(obj);
-    atomic_internal::set_header(vec, buffer);
+    atomic_internal::set_vector_header(vec, buffer);
 
     const auto& values = vec.data;
     auto len = values.size();
     inject_length(len, buffer);
+    writer.write(buffer.data(), buffer.size());
 
     auto ptr = reinterpret_cast<const unsigned char*>(values.data());
-    size_t sofar = buffer.size();
     constexpr size_t width = 16;
-    buffer.insert(buffer.end(), ptr, ptr + len * width);
+    size_t nbytes = len * width;
 
-    // Flipping endianness for each double.
     if (little_endian()) {
-        auto copy = buffer.data() + sofar;
+        buffer.clear();
+        buffer.insert(buffer.end(), ptr, ptr + nbytes);
+        auto copy = buffer.data();
         for (size_t n = 0; n < len * 2; ++n, copy += width / 2) {
             std::reverse(copy, copy + width/2);
         }
+        writer.write(buffer.data(), buffer.size());
+    } else {
+        writer.write(ptr, nbytes);
     }
-
-    writer.write(buffer.data(), buffer.size());
 }
 
 template<class Writer>
 void write_string(const RObject* obj, Writer& writer, std::vector<unsigned char>& buffer) {
     const auto& vec = *reinterpret_cast<const StringVector*>(obj);
-    atomic_internal::set_header(vec, buffer);
+    atomic_internal::set_vector_header(vec, buffer);
 
     const auto& values = vec.data;
     auto len = values.size();
     inject_length(len, buffer);
+    writer.write(buffer.data(), buffer.size());
 
     for (size_t i = 0; i < len; ++i) {
+        buffer.clear();
         write_single_string(vec.data[i], vec.encodings[i], vec.missing[i], buffer);
+        writer.write(buffer.data(), buffer.size());
     }
-
-    writer.write(buffer.data(), buffer.size());
 }
-
 
 }
 
