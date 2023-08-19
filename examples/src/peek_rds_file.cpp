352ece4c1fc99307513990cef6894059b4e8a749
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2023-08-18 23:24:36 -0700

    Enabled and fixed a few compiler warnings. (#7)
    All fixes occur in the testing R package's C++ code. We don't convert
    these into errors because Rcpp has its own warnings beyond our control.
    
    We also added targets to check exhaustively for compiler warnings
    in a pure C++ context with warning->error conversion enabled.

---
+++ examples/src/peek_rds_file.cpp
@@ -0,0 +1,9 @@
+#include "rds2cpp/rds2cpp.hpp"
+#include <string>
+#include <iostream>
+
+int main(int, char* argv[]) {
+    auto finfo = rds2cpp::parse_rds(std::string(argv[1]));
+    std::cout << static_cast<int>(finfo.object->type()) << std::endl;
+    return 0;
+}
